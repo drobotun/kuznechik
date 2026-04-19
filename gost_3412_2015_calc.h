cb502b27d796d847934c54951e5147de7dd89acd
Evgeny Drobotun <evgeny@drobotun.su>
2026-04-19 20:36:53 +0300

    Update gost_3412_2015_calc.h

--- gost_3412_2015_calc.h
+++ gost_3412_2015_calc.h
@@ -1,6 +1,35 @@
 #ifndef GOST_3412_2015_CALC_H
 #define GOST_3412_2015_CALC_H
 
+#include <stdint.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "gost_3412_2015_const.h"
+#include "test.h"
+
+#define BLOCK_SIZE 16
+#define KEY_SIZE 32
+
+typedef uint8_t vect[BLOCK_SIZE]; //блок размером 128 бит
+
+vect iter_C[32]; //итерационные константы C
+
+vect iter_key[10]; //итерационные ключи шифрования
+
+void
+kuznechik_expand_key(const uint8_t* key);
+
+void
+kuznechik_encrypt(const uint8_t* blk, uint8_t* out_blk);
+
+void
+kuznechik_decrypt(const uint8_t* blk, uint8_t* out_blk);
+
+#endif // GOST_3412_2015_CALC_H
+#ifndef GOST_3412_2015_CALC_H
+#define GOST_3412_2015_CALC_H
+
 #include <stdfix.h>
 #include <stdint.h>
 #include <string.h>
